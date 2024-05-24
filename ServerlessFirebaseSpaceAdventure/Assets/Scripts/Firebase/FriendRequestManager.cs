05cf671ba703286247ba625e8a3fc24d038c64e5
Pipe2303 <piperoldanrestrepo@hotmail.com>
2024-05-24 07:42:59 -0500

    FriendRequestFeedback

--- ServerlessFirebaseSpaceAdventure/Assets/Scripts/Firebase/FriendRequestManager.cs
+++ ServerlessFirebaseSpaceAdventure/Assets/Scripts/Firebase/FriendRequestManager.cs
@@ -17,6 +17,8 @@ public class FriendRequestManager : MonoBehaviour
     public GameObject requestItemPrefab;
     [SerializeField] private TMP_InputField _UserInputField;
     public TextMeshProUGUI _usernameText;
+    [SerializeField] private GameObject sucessMessage;
+    [SerializeField] private GameObject errorMessage;
     private void Start()
     {
         databaseReference = FirebaseDatabase.DefaultInstance.RootReference.Child("users");
@@ -40,31 +42,66 @@ public class FriendRequestManager : MonoBehaviour
     }
     public void SendFriendRequest(string receiverId)
     {
-        string requestKey = databaseReference.Child(currentUserId).Child("friendRequests").Child(receiverId).Push().Key;
-        Debug.Log("Request key: " + requestKey);
-        Debug.Log("Current user ID: " + currentUserId);
-        Debug.Log("Receiver ID: " + receiverId);
-        Debug.Log("CurrentUser " + FirebaseAuth.DefaultInstance.CurrentUser);
-        Debug.Log("Database Reference: " + databaseReference.ToString());
-
-        //FriendRequest request = new FriendRequest(currentUserId, receiverId, "pending");
-        /*FriendRequest request = gameObject.AddComponent<FriendRequest>();
-        request.senderId = currentUserId;
-        request.receiverId = receiverId;
-        request.status = "pending";*/
-        Dictionary<string, object> requestData = new Dictionary<string, object>
+        //making sure that the user is not sendinf the request to someone who does not exist
+        databaseReference.Child(receiverId).GetValueAsync().ContinueWithOnMainThread(task =>
         {
-            { "senderId", currentUserId },
-            {"senderUsername", _usernameText.text},
-            { "receiverId", receiverId },
-            { "status", "pending" }
-        };
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Error al obtener el nombre de usuario: " + task.Exception);
+                _UserInputField.text = "";
+                StartCoroutine(ShowErrorMessage());
+                return;
+            }
+            if (task.Result.Value == null)
+            {
+                Debug.LogError("User does not exist");
+                _UserInputField.text = "";
+                StartCoroutine(ShowErrorMessage());
+                return;
+            }
+            else
+            {
+                string requestKey = databaseReference.Child(currentUserId).Child("friendRequests").Child(receiverId).Push().Key;
+                Debug.Log("Request key: " + requestKey);
+                Debug.Log("Current user ID: " + currentUserId);
+                Debug.Log("Receiver ID: " + receiverId);
+                Debug.Log("CurrentUser " + FirebaseAuth.DefaultInstance.CurrentUser);
+                Debug.Log("Database Reference: " + databaseReference.ToString());
+                _UserInputField.text = "";
+                StartCoroutine(ShowSucessMessage());
+                
+                //FriendRequest request = new FriendRequest(currentUserId, receiverId, "pending");
+                /*FriendRequest request = gameObject.AddComponent<FriendRequest>();
+                request.senderId = currentUserId;
+                request.receiverId = receiverId;
+                request.status = "pending";*/
+                Dictionary<string, object> requestData = new Dictionary<string, object>
+                {
+                    { "senderId", currentUserId },
+                    {"senderUsername", _usernameText.text},
+                    { "receiverId", receiverId },
+                    { "status", "pending" }
+                };
 
-        //string jsonRequest = JsonUtility.ToJson(request);
+                //string jsonRequest = JsonUtility.ToJson(request);
 
-        databaseReference.Child(receiverId).Child("friendRequests").Child(requestKey).SetValueAsync(requestData);
+                databaseReference.Child(receiverId).Child("friendRequests").Child(requestKey).SetValueAsync(requestData);
+            }
+        });
+        
     }
-
+    IEnumerator ShowErrorMessage()
+    {
+        errorMessage.SetActive(true);
+        yield return new WaitForSeconds(2);
+        errorMessage.SetActive(false);
+    }
+    IEnumerator ShowSucessMessage()
+    {
+        sucessMessage.SetActive(true);
+        yield return new WaitForSeconds(2);
+        sucessMessage.SetActive(false);
+    }
     public void AcceptFriendRequest(string senderId, string requestKey)
     {
         databaseReference.Child(currentUserId).Child("friendRequests").Child(requestKey).Child("status").SetValueAsync("accepted");
